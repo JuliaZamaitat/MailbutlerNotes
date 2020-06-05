5c578a56027c8275d78d2230b1653cd7a787504e
Julia Zamaitat <j.zamaitat@gmail.com>
2020-06-05 10:42:48 +0200

    Installed nodemon and setup  server.

--- main.js
+++ main.js
@@ -8,3 +8,26 @@ const express = require("express"),
   // cookieParser = require("cookie-parser"),
   // connectFlash = require("connect-flash"),
   router = require("./routes/index");
+
+
+app.set("view engine", "ejs");
+app.set("port", process.env.PORT || 4000);
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+
+app.use(express.json());
+app.use(layouts);
+app.use(express.static("public"));
+
+app.use(methodOverride("_method", {
+  methods: ["POST", "GET"]
+}));
+
+app.use("/", router);
+
+app.listen(app.get("port"), () => {
+  console.log(`Server running at http://localhost:${app.get("port")}`);
+});
