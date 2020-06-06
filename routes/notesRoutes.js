8a57528dd39015259870711de506f05f6289b9ef
Julia Zamaitat <j.zamaitat@gmail.com>
2020-06-06 18:35:27 +0200

    Fix.

--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -4,9 +4,7 @@ const router = require("express").Router(),
   notesController = require("../controllers/notesController");
 
 router.use(notesController.verifyJWT);
-
 router.get("/", notesController.index, notesController.indexView);
-
 router.get("/delete/:id", notesController.delete);
 router.post("/:id", notesController.update);
 
